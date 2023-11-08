cf71ca5d2dc5421828bab57767abb47fefe91426
bellatrick <busayosamuel2016@gmail.com>
2023-11-08 13:13:38 +0100

    improvement

--- src/App.js
+++ src/App.js
@@ -14,34 +14,37 @@ import InvoiceReceipt from "./pages/InvoiceReceipt";
 import TransactionReceipt from "./pages/TransactionReceipt";
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/login" element={<Login />} />
-          {/* <Route exact path="/register" element={<Register />} /> */}
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route exact path="/add_shipment" element={<AddShipment />} />
-          <Route exact path="/logs" element={<Logs />} />
-          <Route exact path="/edit_shipment/:id" element={<EditShipment />} />
-          <Route exact path="/invoice/:id" element={<Invoice />} />
-          <Route exact path="/receipt" element={<ShowReceipt />} />
-          <Route exact path="/invoice_receipt" element={<InvoiceReceipt />} />
-          <Route exact path="/transaction_receipt" element={<TransactionReceipt />} />
-        </Routes>
-      </Router>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={true}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+    <div>
+      
     </div>
+    // <div className="App">
+    //   <Router>
+    //     <Routes>
+    //       <Route exact path="/" element={<LandingPage />} />
+    //       <Route exact path="/login" element={<Login />} />
+    //       {/* <Route exact path="/register" element={<Register />} /> */}
+    //       <Route exact path="/dashboard" element={<Dashboard />} />
+    //       <Route exact path="/add_shipment" element={<AddShipment />} />
+    //       <Route exact path="/logs" element={<Logs />} />
+    //       <Route exact path="/edit_shipment/:id" element={<EditShipment />} />
+    //       <Route exact path="/invoice/:id" element={<Invoice />} />
+    //       <Route exact path="/receipt" element={<ShowReceipt />} />
+    //       <Route exact path="/invoice_receipt" element={<InvoiceReceipt />} />
+    //       <Route exact path="/transaction_receipt" element={<TransactionReceipt />} />
+    //     </Routes>
+    //   </Router>
+    //   <ToastContainer
+    //     position="top-center"
+    //     autoClose={5000}
+    //     hideProgressBar={true}
+    //     newestOnTop={false}
+    //     closeOnClick
+    //     rtl={false}
+    //     pauseOnFocusLoss
+    //     draggable
+    //     pauseOnHover
+    //   />
+    // </div>
   );
 }
 
